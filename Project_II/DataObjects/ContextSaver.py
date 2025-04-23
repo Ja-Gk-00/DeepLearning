343c0a637b39c52742fb7e2c59519fd4f2d76c1d
JA-GK-00 <aaf6@o2.pl>
2025-04-23 15:02:23 +0200

    Add Experiments, Experiment Factory and Context Saver

---
+++ Project_II/DataObjects/ContextSaver.py
@@ -0,0 +1,33 @@
+from io import StringIO
+import sys
+
+class Capturing:
+    """
+    Context manager to capture stdout output and save it to a log file.
+
+    Usage:
+        with Capturing(log_path) as output_lines:
+            # code that prints
+        # printed lines saved to log_path, and output_lines is a list of lines.
+    """
+    def __init__(self, log_path: str):
+        self.log_path = log_path
+        self._stringio = None
+        self._stdout = None
+        self.output_lines: list[str] = []
+
+    def __enter__(self):
+        self._stdout = sys.stdout
+        self._stringio = StringIO()
+        sys.stdout = self._stringio
+        return self.output_lines
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        # Retrieve printed text
+        text = self._stringio.getvalue()
+        self.output_lines.extend(text.splitlines())
+        # Write to log file
+        with open(self.log_path, 'w') as f:
+            f.write(text)
+        sys.stdout = self._stdout
+        del self._stringio
